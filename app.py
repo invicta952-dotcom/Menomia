f7c4a746074d4d8c16313a579fbc22a5ffd1966e
kk <invicta952@gmail.com>
2026-03-16 23:40:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ import requests
 import memeflask
 app = Flask(__name__)
 @app.route("/")
-def index():
+def home():
     return render_template('main.html')
 
-@app.route('/meme')  # This must match what you typed in the browser
-def get_meme():
-    # Your logic to get the meme using memeflask.py
+@app.route("/meme") 
+def index():
     return render_template('meme_index.html', meme_image=meme_url) 
 
 
