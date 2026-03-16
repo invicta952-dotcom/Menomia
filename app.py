9709479b4f47d74694bea4155df5a499b892275b
kk <invicta952@gmail.com>
2026-03-16 19:24:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('main.html')
 
+@app.route(/meme)
+def meme():
+    return render_template('meme_index.html)
+
 if __name__ == "__main__":
     app.run()
 
