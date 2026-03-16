553d5d6d7d21db27f83a42874c45782795f6a932
kk <invicta952@gmail.com>
2026-03-16 18:42:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def index():
-    return "DRINK MORE MONSTERS"
+    return render_template('main.html')
 
 
 app.run(host="0.0.0.0", port=80)
